20412711561682af7f82d0c0ba7d9848df64c9db
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2020-05-13 09:28:06 +0200

    first exercise ready

---
+++ PB-language-statements/index.js
@@ -0,0 +1,12 @@
+console.log('hello world';
+// console.log('This is my first js file');
+// Ctrl + Shift + 7
+
+/* console.log('This is my first js file'); */
+// Ctrl + Shift + A
+
+/* 
+    1. No it doesn't run through 
+    2. Yes, there is an Error called SyntaxError:
+    SyntaxError: missing ) after argument list 
+*/
